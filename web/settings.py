b26ae25c466f839b818d23215b070e699820e120
DaEun <ehsjt3868@gamil.com>
2024-12-06 10:25:03 +0900

    Collect static files

--- web/settings.py
+++ web/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'myapp.apps.MyappConfig',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
